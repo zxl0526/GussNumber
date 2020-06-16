c5a2ca30acf343dfe5f52f9c6cfcbfde30276755
zxl0526 <851450219@qq.com>
2020-06-16 20:52:53 +0800

    20200616_测试类公用方法整合，增加输入格式校验

--- src/main/java/com/twschool/practice/Game.java
+++ src/main/java/com/twschool/practice/Game.java
@@ -1,12 +1,31 @@
 package com.twschool.practice;
 
 public class Game {
+    private static boolean judgeInput(String input) {
+        input = input.replace(" ", "");
+        if(input.length() != 4) {
+            return false;
+        }
 
+        //相同字符串去重
+        StringBuffer sb = new StringBuffer(input);
 
+        String rs = sb.reverse().toString().replaceAll("(.)(?=.*\\1)", "");
 
+        StringBuffer out = new StringBuffer(rs);
+        String result = out.reverse().toString();
+        if(result.length() != 4) {
+            return false;
+        }
+
+        return true;
+    }
 
     public static String playGame(String input) throws Exception {
-         String[] answer = { "1", "2", "3", "4" };
+        if(!judgeInput(input)) {
+            return "error";
+        }
+        String[] answer = { "1", "2", "3", "4" };
         // 值正确，位置不正确
         int answerCountRight = 0;
         // 值正确，位置正确
--- src/test/java/com/twschool/practice/GuessNumberTest.java
+++ src/test/java/com/twschool/practice/GuessNumberTest.java
@@ -11,89 +11,36 @@ public class GuessNumberTest {
     //位置和数字全对（4A0B）
     @Test
     public void should_return_4A0B_input_1234() {
-        //given
-        String input = "1 2 3 4";
-        //when
-        String result = null;
-        try {
-            result = Game.playGame(input);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        //then
-        assertEquals("4A0B", result);
+       should_try_catch("1 2 3 4","4A0B");
     }
     //位置和数字全错（0A0B）
     @Test
     public void should_return_0A0B_input_5678() {
-        //given
-        String input = "5678";
-        //when
-        String result = null;
-        try {
-            result = Game.playGame(input);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        //then
-        assertEquals("0A0B", result);
+        should_try_catch("5 6 7 8","0A0B");
     }
     //位置全错，数字全对（0A4B）
     @Test
     public void should_return_0A4B_input_4321() {
-        //given
-        String input = "4 3 2 1";
-        //when
-        String result = null;
-        try {
-            result = Game.playGame(input);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        //then
-        assertEquals("0A4B", result);
+      should_try_catch("4 3 2 1","0A4B");
     }
     //数字全对，位置有对有错（2A2B）
     @Test
     public void should_return_2A2B_input_1243() {
-        //given
-        String input = "1 2 4 3";
-        //when
-        String result = null;
-        try {
-            result = Game.playGame(input);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        //then
-        assertEquals("2A2B", result);
+        should_try_catch("1 2 4 3","2A2B");
+
     }
     //位置全错，数字有对有错（0A2B）
     @Test
     public void should_return_0A2B_input_2178() {
-        //given
-        String input = "2 1 7 8";
-        //when
-        String result = null;
-        try {
-            result = Game.playGame(input);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        //then
-        assertEquals("0A2B", result);
+        should_try_catch("2 1 7 8","0A2B");
     }
     //位置部分错，数字部分错（1A2B）
     @Test
     public void should_return_1A2B_input_8324() {
-        //given
-        String input = "8 3 2 4";
-        //when
+        should_try_catch("8 3 2 4","1A2B");
+    }
+    public void  should_try_catch(String input,String resultA){
+
         String result = null;
         try {
             result = Game.playGame(input);
@@ -101,8 +48,6 @@ public class GuessNumberTest {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        //then
-        assertEquals("1A2B", result);
+        assertEquals(resultA,result);
     }
-
 }
